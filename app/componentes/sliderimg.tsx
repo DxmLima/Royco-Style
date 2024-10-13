e27ebea9ee5d77475ae37b6b5aed6153cd07ff3f
Mateus Lima <65178646+DxmLima@users.noreply.github.com>
2024-10-13 13:01:58 -0300

    add mobile

--- app/componentes/sliderimg.tsx
+++ app/componentes/sliderimg.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from "next/image"
 
 import Woman from '@/public/Woman.jpg'
+import MobileView from '@/public/MobileView.png'
 
 function CarouselHero () {
   return (
@@ -9,8 +10,15 @@ function CarouselHero () {
     <Image
       src={Woman}
       alt="Picture of the author"
-      className='h-[800px] w-full bg-cover'
+      className='h-[800px] w-full bg-cover invisible md:visible'
     />
+
+<Image
+      src={MobileView}
+      alt="Picture of the author"
+      className='h-[600px] w-auto bg-cover visible md:invisible absolute mb-6'
+    />
+      
     </main>
   )
 }
